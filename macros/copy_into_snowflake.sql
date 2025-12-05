16a97e4e52c603a6e4b35abdb808a7e445139155
Collin Hicks <hickscw2014@gmail.com>
2025-12-05 19:53:57 +0000

    working

--- macros/copy_into_snowflake.sql
+++ macros/copy_into_snowflake.sql
@@ -1,30 +1,34 @@
-{% macro generate_schema_name(custom_schema_name, node) -%}
+{% macro copy_json(table_nm) %}
 
  
 
-    {%- set default_schema = target.schema -%}
+--Delete the data from the copy table before running the copy command
 
-    {%- if custom_schema_name is none -%}
-
- 
-
-        {{ default_schema }}
+delete from {{var ('target_db') }}.{{var ('target_schema')}}.{{ table_nm }};
 
  
 
-    {%- else -%}
+--Copy the data from the snowflake external stage to snowflake table
+
+COPY INTO {{var ('target_db') }}.{{var ('target_schema')}}.{{ table_nm }}
+
+FROM
+
+(
+
+SELECT
+
+$1 AS DATA
+
+FROM @{{ var('stage_name') }}
+
+)
+
+FILE_FORMAT = (TYPE = JSON)
+
+FORCE = TRUE;
 
  
 
-        {{ custom_schema_name | trim }}
-
- 
-
-    {%- endif -%}
-
- 
-
-{%- endmacro %}
-
-
+{% endmacro %}
 
